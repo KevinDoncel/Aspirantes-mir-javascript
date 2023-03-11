51a7005ec9abe592381a6d2bf036783a56d847f4
KevinDoncel <kdoncel1126@gmail.com>
2023-03-10 22:48:11 -0500

    correcion ejercicio-2-bmi

--- js-1/IMC.js
+++ js-1/IMC.js
@@ -1,7 +1,11 @@
 function bmi(weight, height) {
+  if (weight <= 0 || height <= 0) return 0;
   return weight / height ** 2;
 }
 
 console.log("your BMI is: " + bmi(65, 1.8));
 console.log("your BMI is: " + bmi(72, 1.6));
 console.log("your BMI is: " + bmi(52, 1.75));
+console.log("your BMI is: " + bmi(0, 1.9));
+console.log("your BMI is: " + bmi(80, 0));
+console.log("your BMI is: " + bmi(65, -1.6));
