c58c3a4e5e805cad890b9e5288dbf9ec88285b5d
KevinDoncel <kdoncel1126@gmail.com>
2023-03-10 19:20:34 -0500

    ejercicio-1-hello.js

--- js-1/hello.js
+++ js-1/hello.js
@@ -1,8 +1,9 @@
 function hello(name) {
-  console.log("Hola " + name + "!");
+  console.log("Hello " + name + "!");
 }
 
 hello("Kevin");
 hello("Hector");
 hello("Ronald");
 hello("Edgar");
+hello("German");
