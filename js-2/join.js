73f26f2adf0b91d3dfaeea7ad6658cac629991c9
KevinDoncel <kdoncel1126@gmail.com>
2023-03-15 08:59:41 -0500

    ejercicio-4-join

---
+++ js-2/join.js
@@ -0,0 +1,13 @@
+function join(arr) {
+  if (arr.length == []) return '""';
+
+  let str = "";
+  for (let word of arr) {
+    str = str + word + " ";
+  }
+  return str;
+}
+
+console.log(join(["Hola", "Mundo"])); // "Hola Mundo"
+console.log(join(["Make", "It", "Real"])); // "Make It Real"
+console.log(join([])); // ""
